a151717f576127b0610652a514ab86c576167a4a
LeticiaFarias <leticia_farias@alu.ufc.br>
2019-09-07 12:56:29 -0300

    Trabalhando com Strings

--- Java Script/Fundamentos/strings.js
+++ Java Script/Fundamentos/strings.js
@@ -8,3 +8,9 @@ console.log(escola.indexOf('3'))
 
 console.log(escola.substring(3))
 console.log(escola.substring(0, 3))
+
+console.log('Escola '.concat(escola).concat("!"))
+console.log('Escola ' + escola + "!")
+console.log(escola.replace(3, 'e'))
+
+console.log('Ana,Maria,Pedro'.split(','))
