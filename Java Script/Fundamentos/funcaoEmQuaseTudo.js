1f79c0668f19e8525566e136267ca2c94850c0f0
leticia <leticia_farias@alu.ufc.br>
2019-11-21 19:16:13 -0300

    Função

--- Java Script/Fundamentos/funcaoEmQuaseTudo.js
+++ Java Script/Fundamentos/funcaoEmQuaseTudo.js
@@ -2,3 +2,5 @@ console.log(typeof Object) /* Quase tudo em JS é tratado como função.
 *O objeto em JS é uma função
 */
 
+class produto { }
+console.log(typeof produto) /* Classes tbm são funções em JS. 
