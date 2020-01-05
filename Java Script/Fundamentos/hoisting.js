b4d1a30af6cb8e7f46ae3a3f25613221068325f7
LeticiaFarias <leticia_farias@alu.ufc.br>
2020-01-05 20:36:57 -0300

    var e let em Loops e Hoisting

---
+++ Java Script/Fundamentos/hoisting.js
@@ -0,0 +1,11 @@
+console.log("a =", a)
+var a = 2
+console.log("a =", a)
+
+function teste() {
+  console.log("b =", b)
+  var b = 4
+  console.log("b =", b)
+}
+
+teste()
---
+++ Java Script/Fundamentos/letEmLoop.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i)
+}
+// Não funciona pois i está fora do bloco
+//console.log("i =", i)
