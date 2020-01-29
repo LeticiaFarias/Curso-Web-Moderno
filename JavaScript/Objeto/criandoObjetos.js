ceea62dc23712a5a822d33355e7decfaa5754c50
LeticiaFarias <leticia_farias@alu.ufc.br>
2020-01-29 14:06:30 -0300

    Criando Objetos

--- JavaScript/Objeto/criandoObjetos.js
+++ JavaScript/Objeto/criandoObjetos.js
@@ -1,1 +1,32 @@
 // Usando a notação literal
+const obj1 = {}
+console.log(obj1)
+
+// Object em JS
+console.log(typeof Object, typeof new Object)
+const obj2 = new Object
+console.log(obj2)
+
+// Funções construtoras
+function Produto(nome, preco, desc) {
+   this.nome = nome
+   this.getPrecoComDesconto = () => {
+      return preco * (1 - desc)
+   }
+}
+
+const p1 = new Produto('Caneta', 7.99, 0.15)
+const p2 = new Produto('Notebook', 2998, 0.25)
+console.log(p1.getPrecoComDesconto(), p2.getPrecoComDesconto())
+
+// Função Factory
+function criarFuncionario(nome, salarioBase, faltas) {
+   return {
+      nome,
+      salarioBase,
+      faltas,
+      getSalaraio() {
+         return (salarioBase / 30) * (30 - faltas)
+      }
+   }
+}
