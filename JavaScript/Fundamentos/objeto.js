38fce593e729e6698aff8871e238734749d75909
LeticiaFarias <leticia_farias@alu.ufc.br>
2021-07-21 20:42:18 -0300

    update fundamentos/objeto

--- JavaScript/Fundamentos/objeto.js
+++ JavaScript/Fundamentos/objeto.js
@@ -1,19 +1,19 @@
 const prod1 = { }  // Chaves representam obejtos, enquanto arrays são representados com colchetes []
-prod1.nome = 'Celular Ultra mega' // Ele criou o nome dinamicamente
-prod1.preco = 498.98
+prod1.name = 'Celular Ultra mega' // Ele criou o nome dinamicamente
+prod1.price = 498.98
 prod1['Desconto legal'] = 0.40  //evitar atributos com espaços
 
 console.log(prod1)
 
 const prod2 = {
-    nome: 'camisa polo',
-    preco: 50.2,
+    name: 'camisa polo',
+    price: 50.2,
     obj: {
-        blabla: 1,
+        atributo: 1,
         obj: {
-            blabla: 2   //objetos aninhados
+            atributo: 2   //objetos aninhados
         }
     }
 }
-
+console.log(prod2)
 console.log(typeof(prod2))
