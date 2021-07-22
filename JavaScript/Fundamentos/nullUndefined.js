61bcb789b32cd1bd16ca81b6f5c9bc5166e92587
LeticiaFarias <leticia_farias@alu.ufc.br>
2021-07-22 15:40:16 -0300

    null & undefined

---
+++ JavaScript/Fundamentos/nullUndefined.js
@@ -0,0 +1,11 @@
+let valor; // não inicializada
+console.log(valor); // undefined
+
+valor = null; //ausência de valor
+console.log(valor); // null
+
+const produto = {};
+console.log(produto);
+
+produto.price = 3.500
+console.log(produto);
--- JavaScript/Fundamentos/objeto2.js
+++ JavaScript/Fundamentos/objeto2.js
@@ -1,10 +1,10 @@
-console.log(typeof Object) // FUNÇÃO
-console.log(typeof new Object) //OBJETO
+console.log(typeof Object); // FUNÇÃO
+console.log(typeof new Object()); //OBJETO
 
-const Cliente = function(){ }
-console.log(typeof Cliente) //FUNÇÃO
-console.log(typeof new Cliente) //OBJETO
+const Cliente = function () {};
+console.log(typeof Cliente); //FUNÇÃO
+console.log(typeof new Cliente()); //OBJETO
 
-class Produto { } // ES 2015 (ES6)
-console.log(typeof Produto) // FUNÇÃO
-console.log(typeof new Produto()) //OBJETO
+class Produto {} // ES 2015 (ES6)
+console.log(typeof Produto); // FUNÇÃO
+console.log(typeof new Produto()); //OBJETO
